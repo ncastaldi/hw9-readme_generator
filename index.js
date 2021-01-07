9ead6edd7e4996fbf1746d2a0067563dbb2b818b
Nathan Castaldi <nathan@castaldi.dev>
2021-01-07 16:38:46 -0500

    initial commit; uploading starter code

---
+++ index.js
@@ -0,0 +1,18 @@
+(function () {
+    const welcomeMsg = `Professional README Generator | Created By Nathan Castaldi ©2021`;
+    console.log(welcomeMsg);
+})();
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
