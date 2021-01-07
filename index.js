7a98a41feed1c0809ed80cd5f89a45a84270c5e2
Nathan Castaldi <nathan@castaldi.dev>
2021-01-07 16:58:26 -0500

    added psudocode

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-(function () {
-    const welcomeMsg = `Professional README Generator | Created By Nathan Castaldi ©2021`;
-    console.log(welcomeMsg);
-})();
-
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -12,7 +9,18 @@ const questions = [];
 function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    // const welcomeMsg = `Professional README Generator | Created By Nathan Castaldi ©2021`;
+    console.log(`Professional README Generator | Created By Nathan Castaldi ©2021`);
+
+    // Create welcome text explaining how to use the app
+
+    // Ask Questions
+
+    // Output to file
+    //writeToFile(x,y)
+
+}
 
 // Function call to initialize app
 init();
