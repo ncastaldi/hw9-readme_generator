1c1ff6c43e5ef20178c7c084e57e3f9473c85d0b
Nathan Castaldi <nathan@castaldi.dev>
2021-01-07 19:02:01 -0500

    removed inital attempt to write a file as it caused app to break; app able to take and display data now

--- index.js
+++ index.js
@@ -6,9 +6,27 @@ const inquirer = require("inquirer");
 const questions = [
     {
         type: "input",
-        message: "What is your GitHub user name?",
+        message: "Enter your GitHub username:",
         name: "userName",
-        default: "ncastaldi"
+        default: "ncastaldi",
+    },
+    {
+        type: "input",
+        message: "Enter the name of the GitHub repo:",
+        name: "repoName",
+        default: "readme_generator",
+    },
+    {
+        type: "input",
+        message: "Enter the title of your project:",
+        name: "projectTitle",
+        default: "Project1",
+    },
+    {
+        type: "input",
+        message: "Enter a description of your project:",
+        name: "projectDescription",
+        default: "Project1",
     }
 ];
 
@@ -18,12 +36,16 @@ function writeToFile(fileName, data) { }
 // TODO: Create a function to initialize app
 function init() {
     // const welcomeMsg = `Professional README Generator | Created By Nathan Castaldi ©2021`;
-    console.log(`Professional README Generator | Created By Nathan Castaldi ©2021`);
+    console.log(`Professional README Generator | Created By Nathan Castaldi ©2021
+    Follow the prompts to create your readme.md file...`);
 
-    inquirer.prompt(questions);
-
+    inquirer
+        .prompt(questions)
+        .then(answers => {
+            console.log(answers.userName);
+        });
     // Output to file
-    //writeToFile(x,y)
+
 
 }
 
