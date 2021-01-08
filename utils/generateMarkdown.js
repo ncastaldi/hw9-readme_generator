498a4442348f4f3fe3c45d672e9fcf352afe287d
Nathan Castaldi <nathan@castaldi.dev>
2021-01-08 07:19:14 -0500

    added switch to renderLicenseBadge to determine which badge to return to generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      break;
+  }
+  return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,8 +19,13 @@ function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  // Deconstruct answer object  
   const { userName, repoName, projectTitle, projectDescription, licenseSelection } = data;
-  return `
+
+  const licenseBadge = renderLicenseBadge(licenseSelection);
+  console.log(licenseBadge);
+
+  return `${licenseBadge}
   # ${data.projectTitle}
 
   ## Description
